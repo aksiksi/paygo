6012ceba0a48b5b7c2d236678c906f10a892947f
Assil Ksiksi <cyph0nik@gmail.com>
2021-07-15 04:14:02 +0000

    Working react-hook-form for validation

--- frontend/paygo/src/card/CardForm.tsx
+++ frontend/paygo/src/card/CardForm.tsx
@@ -1,6 +1,7 @@
 import React, { FormEvent, useState } from "react"
 import { creditCardType } from "card-validator"
 import { cardNumber as cardNumberValidator, CardNumberVerification } from "card-validator/dist/card-number"
+import { FormState, useForm, UseFormRegister } from "react-hook-form"
 
 import { ReactComponent as VisaLogo } from './svgs/visa.svg'
 import { ReactComponent as MastercardLogo } from './svgs/mastercard.svg'
@@ -28,7 +29,11 @@ export enum CardType {
  *
  * @returns The input element for the CVV.
  */
-function CvvInput(props: {cardType: CardType, className?: string}) {
+function CvvInput(props: {
+    cardType: CardType,
+    className?: string
+    register: UseFormRegister<CardFormFields>,
+}) {
     let maxLength = 3, codeName = "CVV"
 
     if (props.cardType === CardType.Amex) {
@@ -42,6 +47,7 @@ function CvvInput(props: {cardType: CardType, className?: string}) {
         className={props.className}
         type="text"
         id="creditCardCvv"
+        {...props.register("creditCardCvv", { required: true })}
     />
 }
 
@@ -77,17 +83,19 @@ function CardInput(
     props: {
         cardType: CardType,
         cardIsValid?: boolean,
+        register: UseFormRegister<CardFormFields>,
+        formState: FormState<CardFormFields>,
         onCardNumberChange: (_: FormEvent) => void
     }) {
     // Default input border is gray
     let border: string = "border border-gray-300 focus:border-yellow-500 focus:ring-yellow-500" 
 
     // Highlight the credit card input based on detected type and validity
-    if (props.cardIsValid && props.cardType !== CardType.Other) {
-        border = "border-2 border-green-300 focus:border-green-300 focus:ring-green-300"
-    } else if (!props.cardIsValid && props.cardType === CardType.Invalid) {
-        border = "border-2 border-red-300 focus:border-red-300 focus:ring-red-300"
-    }
+    // if (props.cardIsValid && props.cardType !== CardType.Other) {
+    //     border = "border-2 border-green-300 focus:border-green-300 focus:ring-green-300"
+    // } else if (!props.cardIsValid && props.cardType === CardType.Invalid) {
+    //     border = "border-2 border-red-300 focus:border-red-300 focus:ring-red-300"
+    // }
 
     return (
         <div>
@@ -95,7 +103,20 @@ function CardInput(
                 <label>Credit card</label>
                 <span className="block relative">
                     <CardLogo cardType={props.cardType} />
-                    <input type="text" placeholder="4111 1111 1111 1111" className={`mt-1 block pr-12 w-full rounded-sm rounded-t-lg shadow-sm ${border}`} onInput={props.onCardNumberChange} />
+                    <input
+                        type="text"
+                        placeholder="4111 1111 1111 1111"
+                        className={`mt-1 block pr-12 w-full rounded-sm rounded-t-lg shadow-sm ${border}`}
+                        onInput={props.onCardNumberChange}
+                        maxLength={16}
+                        {...props.register("creditCardNumber", {
+                            required: true,
+                            validate: (_: string) => {
+                                return props.cardIsValid
+                            }
+                        })}
+                    />
+                    { props.formState.errors.creditCardNumber && <span className="text-sm text-red-700">Required</span>}
                 </span>
             </div>
             <div className="flex flex-wrap -mx-1">
@@ -103,7 +124,9 @@ function CardInput(
                     <CvvInput
                         cardType={props.cardType}
                         className={"w-full h-10 px-3 text-base rounded-sm rounded-bl-lg placeholder-gray-600 border-gray-300 focus:border-yellow-500 focus:ring-yellow-500"}
+                        register={props.register}
                     />
+                    { props.formState.errors.creditCardCvv && <span className="text-sm text-red-700">Required</span>}
                 </div>
                 <div className="w-1/2 pr-1">
                     <input placeholder="MM/YY" className="w-full h-10 px-3 text-base rounded-sm rounded-br-lg placeholder-gray-600 border-gray-300 focus:border-yellow-500 focus:ring-yellow-500" type="text" id="creditCardExpiry" />
@@ -165,13 +188,25 @@ function getCardType(validityInfo: CardNumberVerification): CardFormState {
     return { cardType, cardIsValid: validityInfo.isValid }
 }
 
+class CardFormFields {
+    creditCardNumber: string = ""
+    creditCardCvv: string = ""
+    creditCardExpiry: string = ""
+    creditCardFirstName: string = ""
+    creditCardLastName: string = ""
+}
+
 export function CardForm() {
     const [state, setState] = useState(new CardFormState())
+
+    const { register, handleSubmit, formState } = useForm({ defaultValues: new CardFormFields() })
+
+    const onSubmit = (data: any) => console.log(data)
 
     return (
         <div className="w-96 m-auto">
             <form className="w-full space-y-2 text-gray-700">
-                <CardInput {...state} onCardNumberChange={(event: FormEvent) => onCardNumberChange(event, setState)} />
+                <CardInput {...state} register={register} formState={formState} onCardNumberChange={(event: FormEvent) => onCardNumberChange(event, setState)} />
 
                 <div className="flex flex-wrap -mx-1 space-y-3 md:space-y-0">
                     <div className="w-full px-1 md:w-1/2">
@@ -185,7 +220,7 @@ export function CardForm() {
                 </div>
 
                 <div>
-                    <button className="mt-3 w-full py-2 px-4 rounded-md justify-center bg-yellow-500 text-white" type="submit">Submit</button>
+                    <button className="mt-3 w-full py-2 px-4 rounded-md justify-center bg-yellow-500 text-white" type="submit" onClick={handleSubmit(onSubmit)}>Submit</button>
                 </div>
             </form>
         </div>
